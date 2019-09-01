978dbc82fc494d2a5cbec21bb2adce02a1c0d799
MrScors <123.18.1.pavlov@gmail.com>
2019-09-01 13:20:54 +0300

    Test for Jenkins added

--- src/test/java/vgwc/service/CounterPickingServiceTest.java
+++ src/test/java/vgwc/service/CounterPickingServiceTest.java
@@ -1,15 +1,13 @@
 package vgwc.service;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
 
 
 public class CounterPickingServiceTest {
 
     @Test
-    public static void createDeployment() {
+    public void createDeployment() {
         assertTrue(true);
     }
 
